0a9f21c3085159461d36618b785d29176cdf4cec
stefanieduarda120-dev <stefanieduarda120@gmail.com>
2026-09-18 07:49:05 -0300

    Add files via upload

---
+++ Aula19 Estruturas compostas 2/main.py
@@ -0,0 +1,17 @@
+jogos = [
+    ["Sally Face", "Undertale", "God Of War"], #Jogos de PC
+    ["TLOU", "Resident Evil", "Davil May Cry"],#Jogos de Play
+    ["It Takes Two", "Mortal Kombat", "Crash"],#Jogos de Xbox
+]
+
+print("JOGOS DE PC")
+for jogo in jogos[0]:
+    print("★", jogo)
+    
+print("\nJOGOS DE PLAYSTATION")
+for jogo in jogos[1]:
+    print("★", jogo)
+    
+print("\nJOGOS DE XBOX")
+for jogo in jogos[2]:
+    print("★", jogo)
