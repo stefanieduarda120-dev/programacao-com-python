cf8f017cf69eb7f4365ad14ca189710e3bd58d5d
stefanieduarda120-dev <stefanieduarda120@gmail.com>
2026-09-18 07:59:04 -0300

    Add files via upload

---
+++ Aula21 Funções/main.py
@@ -0,0 +1,12 @@
+int() #É uma função
+print() #É uma função
+
+#Criando uma função própria
+def saudacao(nome, idade):#Def para criar a função, nome da função,(Parâmetro)
+    print("Olá, seja bem-vindo(a)", nome, "!") #O que a função faz
+    print("Você tem", idade, "anos!")
+    
+saudacao("Kaiser", "30") #Chamando a função, ("Argumento")
+saudacao("Erin", "21")
+saudacao("Kim", "21")
+saudacao("TM", "22")
