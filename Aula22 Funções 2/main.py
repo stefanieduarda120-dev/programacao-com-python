f86886a980a0589bea9676619b044e41c768adae
stefanieduarda120-dev <stefanieduarda120@gmail.com>
2026-09-18 08:00:11 -0300

    Add files via upload

---
+++ Aula22 Funções 2/main.py
@@ -0,0 +1,12 @@
+def somar(n1, n2):
+    print(f"A soma de {n1} e {n2} é igual a {n1 + n2}")
+    return n1 + n2
+    
+print(somar(5, 5))
+
+def calcular_media(nota1, nota2, nota3):
+    media = (nota1 + nota2 + nota3) / 3
+    return media
+    
+print(calcular_media(7, 8, 9))
+print(calcular_media(5, 5, 5))
