cf7bd30a6954e53d3e70fd16bec24bf13bc639e7
stefanieduarda120-dev <stefanieduarda120@gmail.com>
2026-09-18 07:57:39 -0300

    Add files via upload

---
+++ Aula20 Dicionário/main.py
@@ -0,0 +1,11 @@
+personagens = [
+    {"nome": "Sally Face", "idade": 15, "categoria": "jogo"},
+    {"nome": "Cesar Cohen", "idade": 30, "categoria": "rpg"},
+    {"nome": "Baki Hanma", "idade": 17, "categoria": "anime"},
+]
+
+for personagem in personagens:
+    print("Nome: ", personagem["nome"] )
+    print("Idade: ", personagem["idade"] )
+    print("Categoria: ", personagem["categoria"] )
+    print()
